c235088d43fb47b485ac76d5f009b0758263afb6
Julian <julian.thaler06@gmail.com>
2025-03-11 20:10:00 +0100

    Added Enpoints to edit and get Temperatures

--- server.js
+++ server.js
@@ -55,22 +55,22 @@ const connect = () => {
  *         description: Internal server error
  */
 app.post("/steps", authenticate, (req, res) => {
-    const { steps, date, user } = req.body;
-    const connection = connect();
+  const { steps, date, user } = req.body;
+  const connection = connect();
 
-    connection.query(
-        `INSERT INTO t_step (user, date, steps) VALUES (?, ?, ?)`,
-        [user, date, steps],
-        (error, _) => {
-            if (error) {
-                res.status(500).send(error);
-            } else {
-                res.status(201).send("Created");
-            }
-        }
-    );
+  connection.query(
+    `INSERT INTO t_step (user, date, steps) VALUES (?, ?, ?)`,
+    [user, date, steps],
+    (error, _) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(201).send("Created");
+      }
+    }
+  );
 
-    connection.end();
+  connection.end();
 });
 
 /**
@@ -98,22 +98,22 @@ app.post("/steps", authenticate, (req, res) => {
  *         description: Internal server error
  */
 app.get("/steps/one", authenticate, (req, res) => {
-    const { user, date } = req.query;
-    const connection = connect();
+  const { user, date } = req.query;
+  const connection = connect();
 
-    connection.query(
-        `SELECT Steps, Date FROM t_step WHERE user = ? AND date = DATE(?)`,
-        [user, date],
-        (error, results) => {
-            if (error) {
-                res.status(500).send(error);
-            } else {
-                res.status(200).send(results);
-            }
-        }
-    );
+  connection.query(
+    `SELECT Steps, Date FROM t_step WHERE user = ? AND date = DATE(?)`,
+    [user, date],
+    (error, results) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send(results);
+      }
+    }
+  );
 
-    connection.end();
+  connection.end();
 });
 
 /**
@@ -135,22 +135,22 @@ app.get("/steps/one", authenticate, (req, res) => {
  *         description: Internal server error
  */
 app.get("/steps/list", authenticate, (req, res) => {
-    const { user } = req.query;
-    const connection = connect();
+  const { user } = req.query;
+  const connection = connect();
 
-    connection.query(
-        `SELECT Steps, Date FROM t_step WHERE user = ?`,
-        [user],
-        (error, results) => {
-            if (error) {
-                res.status(500).send(error);
-            } else {
-                res.status(200).send(results);
-            }
-        }
-    );
+  connection.query(
+    `SELECT Steps, Date FROM t_step WHERE user = ?`,
+    [user],
+    (error, results) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send(results);
+      }
+    }
+  );
 
-    connection.end();
+  connection.end();
 });
 
 /**
@@ -172,22 +172,22 @@ app.get("/steps/list", authenticate, (req, res) => {
  *         description: Internal server error
  */
 app.get("/steps/total", authenticate, (req, res) => {
-    const { user } = req.query;
-    const connection = connect();
+  const { user } = req.query;
+  const connection = connect();
 
-    connection.query(
-        `SELECT SUM(Steps) AS Total FROM t_step WHERE user = ?`,
-        [user],
-        (error, results) => {
-            if (error) {
-                res.status(500).send(error);
-            } else {
-                res.status(200).send(results[0]);
-            }
-        }
-    );
+  connection.query(
+    `SELECT SUM(Steps) AS Total FROM t_step WHERE user = ?`,
+    [user],
+    (error, results) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send(results[0]);
+      }
+    }
+  );
 
-    connection.end();
+  connection.end();
 });
 
 /**
@@ -215,22 +215,22 @@ app.get("/steps/total", authenticate, (req, res) => {
  *         description: Internal server error
  */
 app.delete("/steps", authenticate, (req, res) => {
-    const { user, date } = req.body;
-    const connection = connect();
+  const { user, date } = req.body;
+  const connection = connect();
 
-    connection.query(
-        `DELETE FROM t_step WHERE user = ? AND date = DATE(?)`,
-        [user, date],
-        (error, _) => {
-            if (error) {
-                res.status(500).send(error);
-            } else {
-                res.status(200).send("Deleted");
-            }
-        }
-    );
+  connection.query(
+    `DELETE FROM t_step WHERE user = ? AND date = DATE(?)`,
+    [user, date],
+    (error, _) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send("Deleted");
+      }
+    }
+  );
 
-    connection.end();
+  connection.end();
 });
 
 /**
@@ -260,24 +260,23 @@ app.delete("/steps", authenticate, (req, res) => {
  *         description: Internal server error
  */
 app.put("/steps", authenticate, (req, res) => {
-    const { user, date, steps } = req.body;
-    const connection = connect();
+  const { user, date, steps } = req.body;
+  const connection = connect();
 
-    connection.query(
-        `UPDATE t_step SET steps = ? WHERE user = ? AND date = DATE(?)`,
-        [steps, user, date],
-        (error, results) => {
-            if (error) {
-                res.status(500).send(error);
-            } else {
-                res.status(200).send(results);
-            }
-        }
-    );
+  connection.query(
+    `UPDATE t_step SET steps = ? WHERE user = ? AND date = DATE(?)`,
+    [steps, user, date],
+    (error, results) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send(results);
+      }
+    }
+  );
 
-    connection.end();
+  connection.end();
 });
-
 
 /**
  * @swagger
@@ -308,24 +307,150 @@ app.put("/steps", authenticate, (req, res) => {
  *         description: Internal server error
  */
 app.post("/temp", authenticate, (req, res) => {
-    const { room, time, temp, humidity } = req.body;
-    const connection = connect();
+  const { room, time, temp, humidity } = req.body;
+  const connection = connect();
 
-    connection.query(
-        'INSERT INTO t_temp (Room, Date, Temp, Humidity) VALUES (?, ?, ?, ?)',
-        [room, time, temp, humidity],
-        (error, _) => {
-            if (error) {
-                res.status(500).send(error);
-            } else {
-                res.status(201).send("Created");
-            }
-        }
-    );
+  connection.query(
+    "INSERT INTO t_temp (Room, Date, Temp, Humidity) VALUES (?, ?, ?, ?)",
+    [room, time, temp, humidity],
+    (error, _) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(201).send("Created");
+      }
+    }
+  );
 
-    connection.end();
+  connection.end();
 });
 
+/**
+ * @swagger
+ * /temp:
+ *   put:
+ *     summary: Update temperature entry
+ *     tags: [Temperature]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               room:
+ *                 type: number
+ *               time:
+ *                 type: string
+ *                 format: date-time
+ *               temp:
+ *                 type: number
+ *               humidity:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Updated
+ *       500:
+ *         description: Internal server error
+ */
+app.put("/temp", authenticate, (req, res) => {
+  const { room, time, temp, humidity } = req.body;
+  const connection = connect();
+
+  connection.query(
+    "UPDATE t_temp SET Temp = ?, Humidity = ? WHERE Room = ? and Date = ?",
+    [temp, humidity, room, time],
+    (error, _) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send("Updated");
+      }
+    }
+  );
+
+  connection.end();
+});
+
+/**
+ * @swagger
+ * /temp/list:
+ *   get:
+ *     summary: Get list of temperature entries for a room
+ *     tags: [Temperature]
+ *     parameters:
+ *       - in: query
+ *         name: room
+ *         schema:
+ *           type: number
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Internal server error
+ */
+app.get("/temp/list", authenticate, (req, res) => {
+  const { room } = req.query;
+  const connection = connect();
+
+  connection.query(
+    `SELECT Temp, Humidity, Date FROM t_temp WHERE Room = ?`,
+    [room],
+    (error, result) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send(result);
+      }
+    }
+  );
+
+  connection.end();
+});
+
+/**
+ * @swagger
+ * /temp/one:
+ *   get:
+ *     summary: Get temperature entry for a specific time
+ *     tags: [Temperature]
+ *     parameters:
+ *       - in: query
+ *         name: room
+ *         schema:
+ *           type: number
+ *         required: true
+ *       - in: query
+ *         name: time
+ *         schema:
+ *           type: string
+ *           format: date-time
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Internal server error
+ */
+app.get("/temp/one", authenticate, (req, res) => {
+  const { room, time } = req.query;
+  const connection = connect();
+
+  connection.query(
+    `SELECT Temp, Humidity, Date FROM t_temp WHERE Room = ? AND Date = ?`,
+    [room, time],
+    (error, result) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send(result);
+      }
+    }
+  );
+
+  connection.end();
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on PORT ${process.env.PORT}`);
